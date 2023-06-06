69a072c3ba73873a93f2b6de7533f98d6da64864
Christopher Landschoot <crlandschoot@gmail.com>
2023-06-05 21:19:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Tiny Audio Diffusion
+This is a repository for generating and training short audio samples on less than 2GB VRAM.
+
+The purpose of this project is to provide access to audio diffusion code for those interested in exploration that have limited resources. Because of the limited sample size, drum samples are a natural fit for training and generating.
+
+The repository is built heavily adapting code from [Archinet's audio-diffusion-pytorch (v0.1.3)](https://github.com/archinetai/audio-diffusion-pytorch)
+
+## Setup
+
+#### Create virtual environment:
+
+Run the following lines from the command line:
+```bash
+conda env create -f environment.yml
+conda activate tiny-audio-diffusion
+```
+
+This will create a conda environment and install the ependincies in the utils/requirements.txt.
+
+
+#### Install Python kernel for Jupyter Notebook
+Run the following line to create a kernel for the current environment to run the inference notebook.
+
+```bash
+python -m ipykernel install --user --name tiny-audio-diffusion --display-name "tiny-audio-diffusion (Python 3.10)"
+```
+
+
+#### Define environment variables
+Rename `.env.tmp` to `.env` and replace the entries with your own variables (example values are random)
+```bash
+DIR_LOGS=/logs
+DIR_DATA=/data
+
+# Required if using wandb logger
+WANDB_PROJECT=audioproject
+WANDB_ENTITY=johndoe
+WANDB_API_KEY=a21dzbqlybbzccqla4txa21dzbqlybbzccqla4tx
+```
+
+## Inference
+Open the `Inference.ipynb` in Jupyter Notebook to generate new drum samples.
+
+## Train
+Run test experiment, see the [`exp`](exp/) folder for other experiments (create your own `.yaml` file there to run a custom experiment!)
+```bash
+python train.py exp=drum_diffusion datamodule.dataset.path=<path/to/your/train/data>
+```
+
+Run on GPU(s)
+
+```bash
+python train.py exp=drum_diffusion trainer.gpus=1 exp=drum_diffusion datamodule.dataset.path=<path/to/your/train/data>
+```
+
+Resume run from a checkpoint
+
+```bash
+python train.py exp=drum_diffusion +ckpt=</path/to/checkpoint.ckpt> exp=drum_diffusion datamodule.dataset.path=<path/to/your/train/data>
+```
---
+++ main/diffusion_module.py
@@ -0,0 +1,277 @@
+from audio_data_pytorch.utils import fractional_random_split
+# from audio_diffusion_pytorch import AudioDiffusionModel, Sampler, Schedule
+# from main import DiffusionModel, Sampler, Schedule
+from pytorch_lightning.loggers import LoggerCollection, WandbLogger
+
+import random
+import warnings
+from copy import deepcopy
+from typing import Any, Callable, Dict, List, Optional, Sequence, Union, Type, Tuple 
+from math import pi 
+
+import json 
+import plotly.graph_objs as go
+import pytorch_lightning as pl
+import torch
+import torchaudio
+import wandb
+import torch.nn.functional as F 
+from einops import rearrange, repeat
+from ema_pytorch import EMA
+from pytorch_lightning import Callback, Trainer
+from pytorch_lightning.utilities import rank_zero_only
+from torch import Tensor, nn, optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm 
+
+# from archisound import ArchiSound
+# from transformers import AutoModel 
+
+""" Model """
+
+# torch.set_float32_matmul_precision('high')
+
+from audio_diffusion_pytorch import UNetV0, VDiffusion, VSampler, LTPlugin, Sampler, Schedule
+
+# Use learned transform to downsapmler (by stride length)
+UNetT_LT = lambda: LTPlugin(UNetV0, num_filters=32, window_length=16, stride=16)
+
+UNetT = lambda: UNetV0
+DiffusionT = VDiffusion
+SamplerT = VSampler
+
+def dropout(proba: float):
+    return random.random() < proba
+
+class Model(pl.LightningModule):
+    def __init__(
+        self,
+        lr: float,
+        lr_beta1: float,
+        lr_beta2: float,
+        lr_eps: float,
+        lr_weight_decay: float,
+        ema_beta: float,
+        ema_power: float,
+        model: nn.Module,
+    ):
+        super().__init__()
+        self.lr = lr
+        self.lr_beta1 = lr_beta1
+        self.lr_beta2 = lr_beta2
+        self.lr_eps = lr_eps
+        self.lr_weight_decay = lr_weight_decay
+        self.model = model
+        self.model_ema = EMA(self.model, beta=ema_beta, power=ema_power)
+
+    @property
+    def device(self):
+        return next(self.model.parameters()).device
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(
+            list(self.model.parameters()),
+            lr=self.lr,
+            betas=(self.lr_beta1, self.lr_beta2),
+            eps=self.lr_eps,
+            weight_decay=self.lr_weight_decay,
+        )
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        wave = batch
+        loss = self.model(wave)
+        self.log("train_loss", loss, sync_dist=True)
+        # Update EMA model and log decay
+        self.model_ema.update()
+        self.log("ema_decay", self.model_ema.get_current_decay(), sync_dist=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        wave = batch
+        loss = self.model_ema(wave)
+        self.log("valid_loss", loss, sync_dist=True)
+        return loss
+
+
+""" Datamodule """
+
+
+class Datamodule(pl.LightningDataModule):
+    def __init__(
+        self,
+        dataset,
+        *,
+        val_split: float,
+        batch_size: int,
+        num_workers: int,
+        pin_memory: bool = False,
+        **kwargs: int,
+    ) -> None:
+        super().__init__()
+        self.dataset = dataset
+        self.val_split = val_split
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+        self.data_train: Any = None
+        self.data_val: Any = None
+
+    def setup(self, stage: Any = None) -> None:
+        split = [1.0 - self.val_split, self.val_split]
+        self.data_train, self.data_val = fractional_random_split(self.dataset, split)
+
+    def get_dataloader(self, dataset) -> DataLoader:
+        return DataLoader(
+            dataset=dataset,            
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=True,
+            prefetch_factor=2,
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        return self.get_dataloader(self.data_train)
+
+    def val_dataloader(self) -> DataLoader:
+        return self.get_dataloader(self.data_val)
+
+
+""" Callbacks """
+
+def get_wandb_logger(trainer: Trainer) -> Optional[WandbLogger]:
+    """Safely get Weights&Biases logger from Trainer."""
+
+    if isinstance(trainer.logger, WandbLogger):
+        return trainer.logger
+
+    if isinstance(trainer.logger, LoggerCollection):
+        for logger in trainer.logger:
+            if isinstance(logger, WandbLogger):
+                return logger
+
+    print("WandbLogger not found.")
+    return None
+
+
+def log_wandb_audio_batch(
+    logger: WandbLogger, id: str, samples: Tensor, sampling_rate: int, caption: str = ""
+):
+    num_items = samples.shape[0]
+    samples = rearrange(samples, "b c t -> b t c").detach().cpu().numpy()
+    logger.log(
+        {
+            f"sample_{idx}_{id}": wandb.Audio(
+                samples[idx],
+                caption=caption,
+                sample_rate=sampling_rate,
+            )
+            for idx in range(num_items)
+        }
+    )
+
+
+def log_wandb_audio_spectrogram(
+    logger: WandbLogger, id: str, samples: Tensor, sampling_rate: int, caption: str = ""
+):
+    num_items = samples.shape[0]
+    samples = samples.detach().cpu()
+    transform = torchaudio.transforms.MelSpectrogram(
+        sample_rate=sampling_rate,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=80,
+        center=True,
+        norm="slaney",
+    )
+
+    def get_spectrogram_image(x):
+        spectrogram = transform(x[0])
+        image = torchaudio.functional.amplitude_to_DB(spectrogram, 1.0, 1e-10, 80.0)
+        trace = [go.Heatmap(z=image, colorscale="viridis")]
+        layout = go.Layout(
+            yaxis=dict(title="Mel Bin (Log Frequency)"),
+            xaxis=dict(title="Frame"),
+            title_font_size=10,
+        )
+        fig = go.Figure(data=trace, layout=layout)
+        return fig
+
+    logger.log(
+        {
+            f"mel_spectrogram_{idx}_{id}": get_spectrogram_image(samples[idx])
+            for idx in range(num_items)
+        }
+    )
+
+
+class SampleLogger(Callback):
+    def __init__(
+        self,
+        num_items: int,
+        channels: int,
+        sampling_rate: int,
+        sampling_steps: List[int],
+        use_ema_model: bool,
+        length: int,
+    ) -> None:
+        self.num_items = num_items
+        self.channels = channels
+        self.sampling_rate = sampling_rate
+        self.sampling_steps = sampling_steps
+        self.use_ema_model = use_ema_model
+        self.log_next = False
+        self.length = length
+
+
+    def on_validation_epoch_start(self, trainer, pl_module):
+        self.log_next = True
+
+    def on_validation_batch_start(
+        self, trainer, pl_module, batch, batch_idx, dataloader_idx
+    ):
+        if self.log_next:
+            self.log_sample(trainer, pl_module, batch)
+            self.log_next = False
+
+    @torch.no_grad()
+    def log_sample(self, trainer, pl_module, batch):
+        is_train = pl_module.training
+        if is_train:
+            pl_module.eval()
+
+        wandb_logger = get_wandb_logger(trainer).experiment
+        model = pl_module.model
+
+        if self.use_ema_model:
+            model = pl_module.model_ema.ema_model
+
+
+        # Get noise for diffusion inference
+        noise = torch.randn(
+            (self.num_items, self.channels, self.length), device=pl_module.device
+        )
+
+        for steps in self.sampling_steps:
+            samples = model.sample(
+                noise,
+                num_steps=steps,
+            )
+            log_wandb_audio_batch(
+                logger=wandb_logger,
+                id="sample",
+                samples=samples,
+                sampling_rate=self.sampling_rate,
+                caption=f"Sampled in {steps} steps",
+            )
+            log_wandb_audio_spectrogram(
+                logger=wandb_logger,
+                id="sample",
+                samples=samples,
+                sampling_rate=self.sampling_rate,
+                caption=f"Sampled in {steps} steps",
+            )
+
+        if is_train:
+            pl_module.train()
